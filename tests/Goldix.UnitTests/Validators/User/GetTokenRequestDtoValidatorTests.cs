8592ae89ad7479983fced1a8fb92332d680091ed
Abolfazl Tayyar <Abolfazltayyar@gmail.com>
2025-10-02 21:16:52 +0330

    feature: add user dto validator tests

---
+++ tests/Goldix.UnitTests/Validators/User/GetTokenRequestDtoValidatorTests.cs
@@ -0,0 +1,83 @@
+﻿using Goldix.Application.Models.User.GetToken;
+using Goldix.Application.Validators.User;
+using Goldix.Domain.Constants;
+
+namespace Goldix.UnitTests.Validators.User;
+
+public class GetTokenRequestDtoValidatorTests
+{
+    private readonly GetTokenRequestDtoValidator _validator;
+
+    public GetTokenRequestDtoValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public async Task Handle_WhenUserNameIsEmpty_ShouldHaveValidationError()
+    {
+        // Arrange
+        var model = new GetTokenRequestDto
+        {
+            UserName = string.Empty,
+            Password = "ValidPassword123!"
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.UserName);
+    }
+
+    [Fact]
+    public async Task Handle_WhenUserNameExceedsMaxLength_ShouldHaveValidationError()
+    {
+        // Arrange
+        var model = new GetTokenRequestDto
+        {
+            UserName = new string('a', DataSchemaConstants.DEFAULT_STRING_LENGTH + 1),
+            Password = "ValidPassword123!"
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.UserName);
+    }
+
+    [Fact]
+    public async Task Handle_WhenPasswordIsEmpty_ShouldHaveValidationError()
+    {
+        // Arrange
+        var model = new GetTokenRequestDto
+        {
+            UserName = "ValidUserName",
+            Password = string.Empty
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Password);
+    }
+
+    [Fact]
+    public async Task Handle_WhenPasswordIsBelowMinLength_ShouldHaveValidationError()
+    {
+        // Arrange
+        var model = new GetTokenRequestDto
+        {
+            UserName = "ValidUserName",
+            Password = new string('a', DataSchemaConstants.DEFAULT_PASSWORD_LENGTH - 1)
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Password);
+    }
+}
---
+++ tests/Goldix.UnitTests/Validators/User/RegisterRequestDtoValidatorTests.cs
@@ -0,0 +1,129 @@
+﻿using Goldix.Application.Models.User.Register;
+using Goldix.Application.Validators.User;
+
+namespace Goldix.UnitTests.Validators.User;
+
+public class RegisterRequestDtoValidatorTests
+{
+    private readonly RegisterRequestDtoValidator _validator;
+
+    public RegisterRequestDtoValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public async Task Handle_WhenPasswordDoesNotMatchRePassword_ShouldHaveValidationError()
+    {
+        // Arrange
+        var model = new RegisterRequestDto
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            PhoneNumber = "1234567890",
+            Password = "Password1!",
+            RePassword = "DifferentPassword1!"
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RePassword);
+    }
+
+
+    [Fact]
+    public async Task Handle_WhenPasswordDoesNotContainsDigits_ShouldHaveValidationError()
+    {
+        // Arrange
+        var model = new RegisterRequestDto
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            PhoneNumber = "1234567890",
+            Password = "Password",
+            RePassword = "Password"
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Password);
+    }
+
+    [Fact]
+    public async Task Handle_WhenPasswordDoesNotContainsLowercase_ShouldHaveValidationError()
+    {
+        // Arrange
+        var model = new RegisterRequestDto
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            PhoneNumber = "1234567890",
+            Password = "PASSWORD1!",
+            RePassword = "PASSWORD1!"
+        };
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Password);
+    }
+
+    [Fact]
+    public async Task Handle_WhenPasswordDoesNotContainsUppercase_ShouldHaveValidationError()
+    {
+        // Arrange
+        var model = new RegisterRequestDto
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            PhoneNumber = "1234567890",
+            Password = "password1!",
+            RePassword = "password1!"
+        };
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Password);
+    }
+
+    [Fact]
+    public async Task Handle_WhenPasswordDoesNotContainsSpecialCharacter_ShouldHaveValidationError()
+    {
+        // Arrange
+        var model = new RegisterRequestDto
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            PhoneNumber = "1234567890",
+            Password = "Password1",
+            RePassword = "Password1"
+        };
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Password);
+    }
+
+    [Fact]
+    public async Task Handle_WhenPasswordIsValid_ShouldNotHaveValidationError()
+    {
+        // Arrange
+        var model = new RegisterRequestDto
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            PhoneNumber = "1234567890",
+            Password = "Password1!",
+            RePassword = "Password1!"
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(model);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
